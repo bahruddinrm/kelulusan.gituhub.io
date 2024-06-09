b3a4323e87a0fd680dd043493f0ecdf57d923fab
Bahruddin <bahruddinrifqi4@gmail.com>
2024-06-09 19:35:25 +0700

    posting

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,11 +5,11 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+// $routes->get('/', 'Home::index');
 $routes->get('/coba', 'Coba::index');
 
 
-$routes->get('/login', 'login::index');
+$routes->get('/', 'login::index');
 // $routes->get('/login/index', 'login::index', ['filter' => 'tamuFilter']);
 $routes->get('/login/main', 'login::main', ['filter' => 'userFilter']);
 // $routes->get('/main/index', 'main::index', ['filter' => 'userFilter']);
